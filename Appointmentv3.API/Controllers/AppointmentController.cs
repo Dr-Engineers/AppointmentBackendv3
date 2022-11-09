55dc84e731a650c493c5484d49db66f779d4a1c1
vinayak131 <vinayakagrawal974@gmail.com>
2022-11-09 11:51:29 +0530

    Appt Card controller

--- Appointmentv3.API/Controllers/AppointmentController.cs
+++ Appointmentv3.API/Controllers/AppointmentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Appointmentv3.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,28 @@ namespace Appointmentv3.API.Controllers
 {
     public class AppointmentController : ApiController
     {
+        IBusinessLayer repo = null;
+
+        public AppointmentController(IBusinessLayer repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAppointmentDetails(int AppointmentId)
+        {
+            var appointmentData = repo.getAppointment(AppointmentId);
+            if (appointmentData == null)
+                return NotFound();
+
+            return Ok(appointmentData);
+        }
+
+
+
+
+
+
+
     }
 }
