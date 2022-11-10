ccb53ba1c2cb4290a87eaa55f6a8b63a130e1bb1
Vachanamruth <gsvachan@gmail.com>
2022-11-10 17:03:39 +0530

    Unit test for GetClinics()

--- UnitTestProject/UnitTest.BusinessLayer.cs
+++ UnitTestProject/UnitTest.BusinessLayer.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Appointmentv3.BL;
+using Appointmentv3.COMMON.Entities.Preset;
+using Appointmentv3.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace UnitTestProject
 {
@@ -7,8 +13,39 @@ namespace UnitTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task TestGetClinics()
         {
+            var mock = new Mock<IAppointmentRepoAsync>();
+            mock.Setup(x => x.getClinicAsync()).ReturnsAsync(new List<Clinic>() 
+            {   new Clinic()
+                {
+                    ClinicID = 1,
+                    ClinicName = "clinicName1"
+                },
+                new Clinic()
+                {
+                    ClinicID = 2,
+                    ClinicName = "clinicName2"
+                } 
+            });
+            IBusinessLayerAsync bl = new BusinessLayerAsync(mock.Object);
+
+            List<Clinic> expected = new List<Clinic>()
+            {   new Clinic()
+                {
+                    ClinicID = 1,
+                    ClinicName = "clinicName1"
+                },
+                new Clinic()
+                {
+                    ClinicID = 2,
+                    ClinicName = "clinicName2"
+                }
+            };
+
+            List<Clinic> actual = await bl.getClinicAsync();
+            Assert.AreEqual(expected, actual);
+
         }
 
 
