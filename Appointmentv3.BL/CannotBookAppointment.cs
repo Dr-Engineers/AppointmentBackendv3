8013570a6b1a628401f28e78fac76fed915f56b5
Vachanamruth <gsvachan@gmail.com>
2022-11-10 16:14:32 +0530

    Created custom Exception

---
+++ Appointmentv3.BL/CannotBookAppointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Appointmentv3.BL
+{
+    [Serializable]
+    internal class CannotBookAppointment : ApplicationException
+    {
+        public CannotBookAppointment()
+        {
+        }
+
+        public CannotBookAppointment(string message) : base(message)
+        {
+        }
+
+        public CannotBookAppointment(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CannotBookAppointment(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
