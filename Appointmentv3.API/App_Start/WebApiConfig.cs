5cec6b38692ed2578735ee8ad6fd238b14df8534
Ayush Singh <Ayush.Singh@skillassure.com>
2022-11-10 12:25:46 +0530

    Added custom exception in clinic controller

--- Appointmentv3.API/App_Start/WebApiConfig.cs
+++ Appointmentv3.API/App_Start/WebApiConfig.cs
@@ -18,6 +18,7 @@ namespace Appointmentv3.API
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
+
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
